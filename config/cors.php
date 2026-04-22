bce44b75f2303c9aa9ddf6aff1695e71f28e21f6
Ivan Alvarez Valencia (el pari) <ivan.alvarez@servimsa.com.mx>
2026-04-22 12:14:03 -0600

    actualizacion

--- config/cors.php
+++ config/cors.php
@@ -2,11 +2,11 @@
 
 return [
 
-    'paths' => ['api/*'],
+    'paths' => ['api/*', 'sanctum/csrf-cookie'],
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:5173'],
+    'allowed_origins' => ['*'], // 👈 temporal (para pruebas)
 
     'allowed_origins_patterns' => [],
 
